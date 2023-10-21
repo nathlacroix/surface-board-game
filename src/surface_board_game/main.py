4912243983534371941ee2ce195dd07f5099e932
Nathan Lacroix <lna@student.ethz.ch>
2023-10-21 16:22:50 +0200

    First prototype of surface board game
     add font for display on screen
     fix typos and update library paths

--- src/surface_board_game/main.py
+++ src/surface_board_game/main.py
@@ -1,11 +1,11 @@
-from surface_board_game.phdhat import PhDHat
+import phdhat
 
-if __name__ == "__main__":
 
-    # Set up system
-    hat = phdhat.PhDHat()
 
-    hat.initial_stage()
-    hat.airbridge_stage()
-    hat.twpa_stage()
-    hat.surface_code_stage()
+# Set up system
+hat = phdhat.PhDHat()
+
+hat.initial_stage()
+hat.airbridge_stage()
+hat.twpa_stage()
+hat.surface_code_stage()
--- src/surface_board_game/phdhat.py
+++ src/surface_board_game/phdhat.py
@@ -1,25 +1,129 @@
 import board
 import busio
 from digitalio import DigitalInOut, Direction, Pull
-from PIL import ImageFont, ImageDraw
+from PIL import ImageFont, ImageDraw, Image
 import adafruit_ssd1306
+import neopixel
+import time
+import numpy as np
+import random
 
 PI_PIN_NEOPIXELS = board.D18
-PI_PIN_AIRBRIDGE = "TODO"
+PI_PIN_AIRBRIDGE = board.D10 #"TODO"
 PI_NEOPIXEL_COUNT = 18
+FONTPATH = "/home/ants/hat/src/surface_board_game/arial.ttf"
 # Tick rate for sleeping between checking the buttons, 60 Hz
 FRAME_TIME = 1.0/60.0
 
+# surface code constants
+N_DETERMINISTIC_SAMPLES = 3
+DISTANCE = 3
+N_AUX_QBS = DISTANCE**2 - 1  # Adjust this based on your actual number of auxiliary qubits
+NEOPIXEL_COUNT = 17  # Adjust this based on your actual number of NeoPixels
+TWPA_CALIBRATED = False
+green_gradients = [
+    (0, 255, 0),
+    (4, 251, 0),
+    (8, 247, 0),
+    (12, 243, 0),
+    (16, 239, 0),
+    (20, 235, 0),
+    (24, 231, 0),
+    (28, 227, 0),
+    (32, 223, 0),
+    (36, 219, 0),
+
+    (44, 211, 0),
+    (48, 207, 0),
+    (52, 203, 0),
+    (56, 199, 0),
+    (60, 195, 0),
+    (64, 191, 0),
+    (68, 187, 0),
+    (72, 183, 0),
+    (76, 179, 0),
+    (80, 175, 0),
+    (84, 171, 0),
+    (88, 167, 0),
+    (92, 163, 0),
+    (96, 159, 0),
+    (100, 155, 0),
+    (104, 151, 0),
+    (108, 147, 0),
+    (112, 143, 0),
+    (116, 139, 0),
+    (120, 135, 0),
+    (124, 131, 0),
+    (128, 127, 0),
+    (132, 123, 0),
+    (136, 119, 0),
+    (140, 115, 0),
+    (144, 111, 0),
+    (148, 107, 0),
+    (152, 103, 0),
+    (156, 99, 0),
+    (160, 95, 0),
+    (164, 91, 0),
+    (168, 87, 0),
+    (172, 83, 0),
+    (176, 79, 0),
+    (180, 75, 0),
+    (184, 71, 0),
+    (188, 67, 0),
+    (192, 63, 0),
+    (196, 59, 0),
+    (200, 55, 0),
+    (204, 51, 0),
+    (208, 47, 0),
+    (212, 43, 0),
+    (216, 39, 0),
+    (220, 35, 0),
+    (224, 31, 0),
+    (228, 27, 0),
+    (232, 23, 0),
+    (236, 19, 0),
+    (240, 15, 0),
+    (244, 11, 0),
+    (248, 7, 0),
+    (255, 3, 0)
+]
+blue_gradients = [
+    (173, 216, 230), (176, 224, 230), (184, 233, 238), (188, 238, 243),
+    (193, 240, 244), (197, 245, 249), (200, 249, 255), (202, 252, 255),
+    (206, 253, 255), (210, 255, 255), (214, 255, 255), (178, 223, 238),
+    (149, 206, 251), (120, 188, 255), (91, 171, 255), (62, 154, 255),
+    (33, 136, 255), (4, 119, 255), (0, 104, 235), (0, 89, 204),
+    (0, 74, 173), (0, 59, 143), (0, 44, 112), (0, 29, 81),
+    (135, 206, 250), (127, 199, 242), (118, 192, 234), (110, 185, 226),
+    (101, 178, 218), (93, 171, 210), (85, 164, 202), (76, 157, 194),
+    (68, 150, 186), (59, 143, 178), (51, 136, 170), (43, 129, 162),
+    (34, 122, 154), (26, 115, 146), (17, 108, 138), (9, 101, 130),
+    (0, 94, 122), (0, 87, 114), (0, 80, 106), (0, 73, 98),
+    (0, 66, 90), (0, 59, 82), (0, 52, 74), (0, 45, 66),
+    (0, 38, 58), (0, 31, 50), (0, 24, 42), (0, 17, 34),
+    (0, 10, 26), (0, 3, 18), (0, 0, 10), (0, 0, 20),
+    (0, 0, 30), (0, 0, 40), (0, 0, 50), (0, 0, 60),
+    (0, 0, 70), (0, 0, 80), (0, 0, 90), (0, 0, 100),
+    (0, 0, 110), (0, 0, 120), (0, 0, 130), (0, 0, 140),
+    (0, 0, 150), (0, 0, 160), (0, 0, 170), (0, 0, 180)
+]
+red_gradients = [(255//3, 0//4, 0//4)]*64
+COLOR_Z_AUX_QB = green_gradients[10]
+COLOR_X_AUX_QB = blue_gradients[20]
+COLOR_DATA_QB = (255//3, 0//4, 0//4)
+# Other global variables
+current_frame = 0
+
 class PhDHat:
 
-    def __init__():
+    def __init__(self):
         self.state = "pre-initialize"
         self.pixels = neopixel.NeoPixel(
             PI_PIN_NEOPIXELS,
             PI_NEOPIXEL_COUNT,
             brightness=0.2,
             auto_write=False,
-            pixel_order=neopixel.RGB
+            pixel_order=neopixel.GRB
         )
         # Create the I2C interface.
         self.i2c = busio.I2C(board.SCL, board.SDA)
@@ -31,9 +135,11 @@ class PhDHat:
             self.disp_height,
             self.i2c,
         )
+        self.draw = None
+        self.image = None
 
         # Font for text display
-        self.font = ImageFont.truetype("arial.ttf", 15)
+        self.font =  ImageFont.truetype(FONTPATH, 15)
 
         # Create the buttons
         # Default state is high (True)
@@ -74,21 +180,61 @@ class PhDHat:
         self.twpa_pump_frequency = 7.0
 
     def _display_text_on_screen(
-        self, text: str
+        self, text: str, new_screen=True, font: ImageFont = None,
+            font_size: None = None, position: tuple = None, anchor="mm",
+            sleep: int = 0
     ) -> None:
-        image = ImageDraw.text(
-            (self.disp_width/2, self.disp_height/2),
+        # Create blank image for drawing.
+        # Make sure to create image with mode '1' for 1-bit color.
+        width = self.disp.width
+        height = self.disp.height
+        if new_screen:
+            self.image = Image.new("1", (width, height))
+
+            # Get drawing object to draw on image.
+            self.draw = ImageDraw.Draw(self.image)
+            # Draw a black filled box to clear the image.
+            self.draw.rectangle((0, 0, width, height), outline=1, fill=1)
+
+        if font_size is not None:
+            font = ImageFont.truetype(FONTPATH, font_size)
+        elif font is None:
+            font = self.font
+        if position is None:
+            position = (self.disp_width / 2, self.disp_height / 2)
+
+        self.draw.text(
+            position,
             text,
-            font=self.font,
-            anchor="mm",
-        ).convert("1")
-        self.disp.image(image)
+            font=font,
+            anchor=anchor,
+        )
+
+        self.disp.image(self.image)
         self.disp.show()
 
+        if sleep:
+            time.sleep(sleep)
+
+    def _display_surface_board_cycle(
+            self, score: int, n_rounds: int, streak: int,
+            cycle: int,
+    ) -> None:
+        text = f"Score: {score}/{n_rounds}      "\
+                f"Streak: {streak}"
+        position=(2, 2)
+        self._display_text_on_screen(text, new_screen=True,
+                                     font_size=10, position=position,
+                                     anchor='lt')
+
+        self._display_text_on_screen(f'Cycle: {cycle}', new_screen=False)
+        # time.sleep(2)
+
     def initial_stage(self):
+        print('Initial stage ...')
         # Display welcome text
         self._display_text_on_screen(
-            "Welcome to your PhD hat\nPress A to start"
+            "Welcome\nto your PhD hat\nPress #5 to start"
         )
         while True:
             # If A button pressed (value brought low)
@@ -99,21 +245,155 @@ class PhDHat:
 
     def airbridge_stage(self):
         # Display message
-         self._display_text_on_screen(
-            "Finish fabricating the airbridges"
+        self._display_text_on_screen(
+            "1. Finish fabricating\nthe airbridges"
         )
         while True:
             # If airbridge connection made (value brought low)
             if not self.airbridge_input.value:
                 return
-            # If A and B pressed (brought low)
-            elif not self.button_a.value and not self.button_b_value:
+            # bypass If A and B pressed (brought low)
+            elif not self.button_a.value and not self.button_b.value:
+                # wait a bit to avoid skipping other steps
+                time.sleep(FRAME_TIME*15)
                 return
             else:
                 time.sleep(FRAME_TIME)
 
     def twpa_stage(self):
-        pass
+        # Display message
+        self._display_text_on_screen(
+            "2. Tune the TWPA!"
+        )
     
     def surface_code_stage(self):
-        pass
+        print('Starting surface code game ...')
+        self._display_text_on_screen(
+            "Starting surface\nboard game..."
+        )
+        time.sleep(2)
+
+        playing = True
+        print('Loading samples...')
+        # Assume your samples are loaded in the following format:
+        # samples = {0: {'syndromes': [...], 'data_qubits': [...], 'log_op': ...}, 1: {...}, ...}
+        samples = self.load_samples("samples.npz")
+        current_round = 1
+        self.score = 0
+        self.streak = 0
+        self.n_rounds = 3
+
+        while playing:
+            print(f'Game #{current_round}')
+            self._display_text_on_screen(f'Game #{current_round}', sleep=2)
+            sample = self.choose_sample(samples, current_round)
+            self.display_syndrome(sample, current_round)
+            # check logical operator
+            success = self.check_logical_operator(sample['log_op'])
+            current_round += 1
+            self.score += success
+            if success:
+                self.streak += 1
+                self.display_success_screen(self.score, self.streak)
+            else:
+                self.streak = 0
+                self.display_failure_screen(self.score)
+
+            if current_round > self.n_rounds:
+                playing = False
+
+        print('Game over.')
+
+    def check_logical_operator(self, log_op):
+        return True
+    @staticmethod
+    def load_samples(file_path, display=False):
+        data = np.load(file_path, allow_pickle=True)
+        samples = {int(seed): data[seed].item() for seed in data.files}
+        if display:
+            for seed, data in samples.items():
+                print(f"Seed {seed}:")
+                print("Syndromes:", data['syndromes'])
+                print("Data qubits:", data['data_qubits'])
+                print("Log op:", data['log_op'])
+                print("\n")
+        return samples
+
+    @staticmethod
+    def choose_sample(samples, current_round):
+        global current_frame
+
+        if current_frame < N_DETERMINISTIC_SAMPLES:
+            # Return the n-th sample for the first N_DETERMINISTIC_SAMPLES rounds
+            sample_index = current_frame
+        else:
+            # Return a random sample after the first N_DETERMINISTIC_SAMPLES rounds
+            sample_index = random.randint(0, len(samples) - 1)
+
+        current_frame += 1
+        return samples[sample_index]
+
+    def display_syndrome(self, sample, current_round, colors=None, bypass_buttons=False):
+        if colors is None:
+            colors = [COLOR_Z_AUX_QB] * 4 + [COLOR_X_AUX_QB] * 4
+
+        cycle = 0
+        exit = False
+        while cycle < len(sample['syndromes']) / N_AUX_QBS  and not exit:
+            syndrome_slice = sample['syndromes'][cycle*N_AUX_QBS: (cycle + 1) * N_AUX_QBS]
+            print("Cycle:", cycle +1, syndrome_slice) # for display, cycles are 1-indexed
+            self._display_surface_board_cycle(score=self.score, n_rounds=self.n_rounds,
+                                              streak=self.streak, cycle=cycle+1) # for display, cycles are 1-indexed
+            self.light_neopixels(syndrome_slice, colors)
+            self.pixels.show()
+            if bypass_buttons:
+                cycle += 1
+                time.sleep(2)
+            else:
+                frame_update = None
+                while frame_update is None:
+                    frame_update = self.check_buttons()
+                    if frame_update == 'next':
+                        cycle += 1
+                    if frame_update == 'prev':
+                        if cycle > 0:
+                            cycle -= 1
+                    if frame_update == 'exit':
+                        exit = True
+                time.sleep(0.5)
+
+        #
+        # return False  # Unsuccessful display
+    def check_buttons(self):
+        if not self.button_r.value:
+            return 'next'
+        elif not self.button_l.value:
+            return 'prev'
+        elif not self.button_a.value and not self.button_b.value:
+            return 'exit'
+    def light_neopixels(self, syndrome_slice, colors):
+        # Adjust this function based on your actual NeoPixel setup
+        for i in range(min(len(syndrome_slice), NEOPIXEL_COUNT)):
+            if syndrome_slice[i]:
+                self.pixels[i] = colors[i]  # Turn on NeoPixel
+            else:
+                self.pixels[i] = (0, 0, 0)  # Turn off NeoPixel
+
+    def display_logical_operator_prompt(self):
+        print(
+            "Do you want to flip the logical operator? (Press 'left' to decline, 'right' to accept)")
+
+
+
+    def display_success_screen(self, score, streak):
+        text = f"Success!\n New Score: {score}, Streak: {streak}"
+        print(text)
+        self._display_text_on_screen(text, font_size=12)
+        time.sleep(2)
+
+    def display_failure_screen(self, score):
+        text = f"Incorrect :-(\nScore: {score}"
+        print(text)
+        self._display_text_on_screen(text, font_size=12)
+        time.sleep(2)
+
